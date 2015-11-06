75f5ca8823afa5b46dd0aefb845d9e2ba208b681
Andrew Shaffer <darawk@gmail.com>
2015-11-06 02:27:40 -0800

    add thunk unrendering and fix hooks

--- src/index.js
+++ src/index.js
@@ -4,36 +4,66 @@
 
 import {actions} from 'virtex'
 import {objectEqual, arrayEqual} from './shallowEqual'
+import uid from 'get-uid'
 
 /**
  * Constants
  */
 
-const {RENDER_THUNK} = actions.types
+const {RENDER_THUNK, UNRENDER_THUNK} = actions.types
 
 /**
  * virtex-component
  */
 
 function middleware ({dispatch}) {
-  return next => action =>
-    action.type === RENDER_THUNK
-      ? component(a => a && dispatch(a), action)
-      : next(action)
+  return next => action => {
+    switch (action.type) {
+      case RENDER_THUNK:
+        return renderComponent(a => a && dispatch(a), action)
+      case UNRENDER_THUNK:
+        return unrenderComponent(a => a && dispatch(a), action)
+      default:
+        return next(action)
+    }
+  }
 }
 
-function component (dispatch, {thunk, prev}) {
+function unrenderComponent (dispatch, {thunk}) {
+  const {beforeUnmount} = thunk.component
+  beforeUnmount && dispatch(beforeUnmount(thunk.props))
+}
+
+function renderComponent (dispatch, {thunk, prev}) {
   if (thunk.vnode) return thunk.vnode
-  const {beforeMount, afterMount, beforeUpdate, afterUpdate} = thunk.component
+  const {beforeMount, beforeUpdate, afterUpdate, afterMount} = thunk.component
 
   if (!prev || !isSameThunk(thunk, prev)) {
-    beforeMount && dispatch(beforeMount(thunk.props))
-    afterMount && setTimeout(() => dispatch(afterMount(thunk.props)))
-    return render(thunk)
+    thunk.id = uid()
+    if (beforeMount) {
+      dispatch(beforeMount(thunk.props))
+    }
+
+    const vnode = thunk.vnode = thunk.component.render(thunk.props)
+
+    if (afterMount) {
+      vnode.attrs = vnode.attrs || {}
+      vnode.attrs[uid() + ':afterMount'] = () => { dispatch(afterMount(thunk.props)) }
+    }
+
+    return vnode
   } else if (shouldUpdate(thunk.component, prev.props, thunk.props)) {
-    beforeUpdate && dispatch(beforeUpdate(prev.props, thunk.props))
-    afterUpdate && setTimeout(() => dispatch(afterUpdate(prev.props, thunk.props)))
-    return render(thunk)
+    if (beforeUpdate) {
+      dispatch(beforeUpdate(prev.props, thunk.props))
+    }
+
+    thunk.vnode = thunk.component.render(thunk.props)
+
+    if (afterUpdate) {
+      dispatch(afterUpdate(prev.props, thunk.props))
+    }
+
+    return thunk.vnode
   } else {
     thunk.vnode = prev.vnode
     return prev.vnode
@@ -48,31 +78,6 @@ function shouldUpdate (component, prevProps, nextProps) {
   return component.shouldUpdate
     ? component.shouldUpdate(prevProps, nextProps)
     : !objectEqual(prevProps, nextProps)
-}
-
-function render (thunk) {
-  thunk.vnode = thunk.component.render(thunk.props)
-
-  return thunk.vnode
-}
-
-function applyHooks (vnode, {key, component, props}) {
-  const attrs = vnode.attrs = vnode.attrs || {}
-
-  // In the case of beforeMount, we only wish to patch in our hook
-  // on the first run so it runs once on creation
-  if (component.afterMount && !attrs[key + ':afterMount']) {
-    attrs[key + ':afterMount'] = (node, name, remove) => remove || dispatch(afterMount(props))
-  }
-
-  // In the case of beforeUnmount, we want to patch it in each time,
-  // so that whenever the component is unmounted, it'll have the
-  // latest props
-  if (component.beforeUnmount) {
-    attrs[key + ':beforeUnmount'] = (node, name, remove) => remove && dispatch(beforeUnmount(props))
-  }
-
-  return vnode
 }
 
 function isSameThunk (a, b) {
